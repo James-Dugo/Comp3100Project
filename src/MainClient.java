5ed2fb068e124bb02caa8d1ba19178d41a4a2eca
James-Dugo <james.dugo@students.mq.edu.au>
2022-05-27 18:17:37 +1000

    adding commands

--- src/MainClient.java
+++ src/MainClient.java
@@ -3,24 +3,57 @@ public class MainClient {
 
     static String hostname;
     static int port;
+    static String algorithm;
+    static Boolean verbose=false;
 
     public static void main(String[] args) {
         
         for(String arg:args){
             System.out.println(arg);
-        }        
-        if(args.length==3){
-            hostname=args[1];
-            port=Integer.parseInt(args[2]);
+        }
 
-            if(args[0].equalsIgnoreCase("fc")){
-                FcClient client=new FcClient(hostname,port);
-                client.mainLoop();
-            }else if(args[0].equalsIgnoreCase("lrr")){
-                LrrClient client=new LrrClient(hostname,port);
-                client.mainLoop();
+        if(args.length%2!=0){usage();}
+        /**
+         * Goes through the arguments and sets the hostname,port and algorithm based on them
+         * -u [hostname:port] -a [fc|lrr] defaults 
+         */
+        for(int i=0;i<args.length;i++){
+            switch(args[i]){
+                case "-u":
+                    String[] temp=args[i+1].split(":");
+                    hostname=temp[0];
+                    port=Integer.parseInt(temp[1]);
+                    i++;
+                    break;
+                case "-a":
+                    algorithm=args[i+1];
+                    i++;
+                    break;
+                case "-v":
+                    verbose=true;
+                    i++;
+                    break;
+                default:
+                    System.err.println("Using default");
+                    algorithm="tt";
+                    hostname="localhost";
+                    port=50000;
             }
-        } else {usage();}
+        }
+
+        hostname=args[1];
+        port=Integer.parseInt(args[2]);
+
+        if(algorithm.equalsIgnoreCase("fc")){
+            FcClient client=new FcClient(hostname,port);
+            client.mainLoop();
+        }else if(algorithm.equalsIgnoreCase("lrr")){
+            LrrClient client=new LrrClient(hostname,port);
+            client.mainLoop();
+        }else if(algorithm.equalsIgnoreCase("tt")){
+            TtClient client=new TtClient(hostname,port);
+            client.mainLoop();
+        }
     }
 
     private static void usage(){
